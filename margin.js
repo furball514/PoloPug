2bce885b1aac011e1e3c5ffe5c001c0540a53369
furball514 <karthik.nallasivan@gmail.com>
2017-06-12 16:58:58 +0530

    4:58

--- margin.js
+++ margin.js
@@ -1,27 +1,40 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, WebView } from "react-native";
-import { Container, Header, Content, Button } from "native-base";
+import { StyleSheet, Text } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Item,
+  Input,
+  List,
+  ListItem,
+  H3,
+  Body
+} from "native-base";
 import axios from "axios";
 
 export default class Margin extends React.Component {
   state = {
-    data: []
+    data: [],
+    text: "BTC"
   };
 
-  componentDidMount() {
-    this.getData("BTC");
-  }
-
-  getData(currency) {
+  getData() {
     axios
       .get(
-        `https://poloniex.com/public?command=returnLoanOrders&currency=${currency}`
+        `https://poloniex.com/public?command=returnLoanOrders&currency=${encodeURIComponent(
+          this.state.text
+        )}`
       )
       .then(data => {
         this.setState({
-          data: JSON.stringify(data.data)
+          data: data.data
         });
       });
+  }
+
+  componentDidMount() {
+    this.getData();
   }
 
   render() {
@@ -31,20 +44,65 @@ export default class Margin extends React.Component {
           <Text style={styles.text}> Coming Soon </Text>
         </Header>
         <Content>
-          <TextInput
-            style={styles.content}
-            onSubmitEditing={currency => {
-              this.getData(currency);
+          <Item rounded>
+            <Input
+              style={{ color: "white" }}
+              placeholder="Enter Currency Pair"
+              placeholderTextColor="white"
+              maxLength={5}
+              autoCapitalize="characters"
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+              onSubmitEditing={e => {
+                this.getData();
+              }}
+              returnKeyType="go"
+            />
+          </Item>
+          <H3 style={{ color: "white" }}> OFFERS </H3>
+          <List
+            dataArray={this.state.data.offers}
+            renderHeader={() =>
+              <Text style={{ flexDirection: "row" }}>
+                <Text style={{ flex: 1 }}> Rate </Text>
+                <Text style={{ flex: 1 }}> Amount </Text>
+                <Text style={{ flex: 1 }}> RangeMin </Text>
+                <Text style={{ flex: 1 }}> RangeMax </Text>
+              </Text>}
+            renderRow={offer => {
+              <ListItem>
+                <Body>
+                  <Text style={styles.content}>
+                    {offer.rate}
+                  </Text>
+                </Body>
+              </ListItem>;
             }}
-            returnKeyType="go"
-            autoCapitalize="characters"
-            maxLength={5}
           />
-          <Text style={styles.content}> {this.state.data} </Text>
-          <WebView
+          <H3 style={{ color: "white" }}> DEMANDS </H3>
+          <List
+            dataArray={this.state.data.demands}
+            renderHeader={() =>
+              <Text style={{ flexDirection: "row" }}>
+                <Text style={{ flex: 1 }}> Rate </Text>
+                <Text style={{ flex: 1 }}> Amount </Text>
+                <Text style={{ flex: 1 }}> RangeMin </Text>
+                <Text style={{ flex: 1 }}> RangeMax </Text>
+              </Text>}
+            renderRow={demand => {
+              <ListItem>
+                <Body>
+                  <Text style={styles.content}>
+                    {demand.rate}
+                  </Text>
+                </Body>
+              </ListItem>;
+            }}
+          />
+          {/*<WebView
             style={{ height: 900, alignSelf: "stretch" }}
             source={{ uri: "https://m.poloniex.com/#/marginTrading" }}
-          />
+          />*/}
         </Content>
       </Container>
     );
